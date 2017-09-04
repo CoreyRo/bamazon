0adbc5cd9dc0498f2f7e9724999c481536f31132
Corey Rodems <coreyrodems@gmail.com>
2017-09-04 16:11:15 -0700

    addded table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,8 +2,12 @@ var SQL = require("./sqlFile.js");
 var inquierer = require("inquirer");
 var consoleTable = require("console.table");
 var dbSettings = require("./dbSettings");
-var db = new SQL(dbSettings.HOST, dbSettings.USER, dbSettings.PASS, dbSettings.PORT, "bamazon_db")
-console.log(dbSettings.HOST)
+
+//Insert your MySQL config.
+//var db = new SQL(HOST,USER,PASS,PORT,DATABSE)
+//Replace dbSettings.X with your SQL settings
+var database = new SQL("localhost", "root", "root", 3306, "bamazon_db");
+database.IsConnected();
 inquierer
     .prompt([
         {
@@ -14,5 +18,22 @@ inquierer
         }
     ])
     .then(function(answers){
-        
+        database.Get("products")
+        .then(function(products){
+            var tableArr = [];
+            for (var i = 0; i < products.length; i++) {
+                resultObj = {
+                    "Item ID": products[i].item_id,
+                    "Product": products[i].product_name,
+                    "Department": products[i].dept_name,
+                    "Price (USD)": " $ " + products[i].cust_price,
+                    "Stock": products[i].stock_qty
+                }
+                tableArr.push(resultObj)
+            }
+            console.table(tableArr)
+
+            database.quitApp();
+        });
+       
     })
--- misc/script.sql
+++ misc/script.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
     item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(101) NOT NULL,
     dept_name VARCHAR(101) NOT NULL,
-    cust_price INTEGER(10) NOT NULL,
+    cust_price FLOAT(11) NOT NULL,
     stock_qty INTEGER(10) NOT NULL,
     PRIMARY KEY (item_id)
 );
@@ -16,21 +16,21 @@ CREATE TABLE products (
 CREATE TABLE depts (
     dept_id INTEGER(11) AUTO_INCREMENT NOT NULL,
     dept_name VARCHAR(101) NOT NULL,
-    over_head_costs INTEGER(10) NOT NULL,
+    over_head_costs FLOAT(10) NOT NULL,
     PRIMARY KEY (dept_id)
 );
 
 /*Test Values*/
 INSERT INTO products (product_name, dept_name, cust_price, stock_qty) 
 VALUES 
-('Gaming Mouse', 'Electronics', 60, 800),
-('24" IPS Monitor', 'Electronics', 200, 125),
-('Golden Toaster', 'Appliances', 5000, 10),
-('40lb Dog Food', 'Pet Supplies', 65, 500),
-('Rubber Chew Toy', 'Pet Supplies', 8, 900),
-('Microwave', 'Appliances', 35, 320),
-('Food Processor', 'Appliances', 80, 550),
-('75" TV', 'Electronics', 600, 255),
-('Patio Umbrella', 'Outdoor/Garden', 125, 175),
-('Deck Chair', 'Outdoor/Garden', 40, 400);
+('Gaming Mouse', 'Electronics', 59.99, 800),
+('24" IPS Monitor', 'Electronics', 229.99, 125),
+('Golden Toaster', 'Appliances', 4999.99, 10),
+('40lb Dog Food', 'Pet Supplies', 54.99, 500),
+('Rubber Chew Toy', 'Pet Supplies', 9.99, 900),
+('Microwave', 'Appliances', 34.99, 320),
+('Food Processor', 'Appliances', 79.99, 550),
+('75" TV', 'Electronics', 799.99, 255),
+('Patio Umbrella', 'Outdoor/Garden', 124.99, 175),
+('Deck Chair', 'Outdoor/Garden', 34.99, 400);
 

b5794637876d652b9631ff07823fe0cc1b6b00f2
Corey Rodems <coreyrodems@gmail.com>
2017-09-09 09:32:51 -0700

    Update bamazonSupervisor.js

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -12,6 +12,9 @@ var consoleTable = require("console.table");
 var dbConfig = require("./dbSettings");
 var database = new SQL(dbConfig.HOST, dbConfig.USER, dbConfig.PASS, dbConfig.PORT, dbConfig.DB);
 
+
+//Having trouble understanding this still
+
 // SELECT 
 // products.dept_name, SUM(products.sales) AS total_sales
 // FROM
@@ -20,3 +23,9 @@ var database = new SQL(dbConfig.HOST, dbConfig.USER, dbConfig.PASS, dbConfig.POR
 // depts ON products.dept_name = depts.dept_name
 // GROUP BY dept_name
 // order by total_sales;;
+
+//Get the total sales from each department
+
+//Display the data in a table by department
+
+//Compare data to overhead costs
